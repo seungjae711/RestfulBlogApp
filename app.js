ea5a221256d00178ea99fc9dd73eca28ad99d801
Sungjae Choi <seungjae711@hotmail.com>
2019-09-17 19:28:42 -0700

    Edit Update Delete and styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express     = require("express"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     app         = express();
@@ -8,6 +10,8 @@ mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 // MONGGOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -48,6 +52,7 @@ app.get("/blogs/new", function(req, res){
     res.render("new");
 })
 
+
 // CREATE
 app.post("/blogs", function(req, res){
     Blog.create(req.body.blog, function(err, newBlog){
@@ -59,6 +64,17 @@ app.post("/blogs", function(req, res){
     })
 })
 
+// EDIT 
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
 // SHOW
 app.get("/blogs/:id", function(req, res) {
     Blog.findById(req.params.id, function(err, foundBlog){
@@ -67,8 +83,32 @@ app.get("/blogs/:id", function(req, res) {
         }else {
             res.render("show", {blog: foundBlog});
         }
+    });
+});
+
+
+// UPDATE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+}); 
+
+// DELETE
+app.delete("/blogs/:id", function(req, res){
+    // destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else {
+            res.redirect("/blogs");
+        }
     })
-})
+});
 
 app.listen(3000, function() {
     console.log("SERVER IS RUNNING")
